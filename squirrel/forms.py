9ebabb25515d00ee1b5987f21c45421babd0da12
Tian Xia <tx2206@columbia.edu>
2020-10-21 03:19:59 +0000

    Add views and htmls

---
+++ squirrel/forms.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm
+
+from .models import Squirrel
+
+class SquirrelForm(ModelForm):
+    class Meta:
+        model = Squirrel
+        fields = (
+                'X',
+                'Y',
+                'Unique_Squirrel_ID',
+                'Shift',
+                'Date',
+                'Age',
+                'Primary_Fur_Color',
+                'Location',
+                'Specific_Location',
+                'Running',
+                'Chasing',
+                'Climbing',
+                'Eating',
+                'Foraging',
+                'Other_Activities',
+                'Kuks',
+                'Quaas',
+                'Moans',
+                'Tail_flags',
+                'Tail_twitches',
+                'Approaches',
+                'Indifferent',
+                'Runs_from',
+
+        )
--- squirrel/urls.py
+++ squirrel/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'squirrel'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.home, name='home'),
+    path('map',views.map, name='map'),
+    path('sightings', views.sightings, name='sightings'),
+    path('stats', views.stats, name='stats'),
+    path('add', views.add, name='add'),
+
 ]
 
